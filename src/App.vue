358765912686c554a9aacc3c4ed130e65ee6f277
Aurélien Lonni <aurelien.lonni@gmail.com>
2021-05-04 17:42:35 +0200

    Creation of a simple counter component.

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,21 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hr>
+    <Counter></Counter>
+    <hr>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
+import Counter from './components/Counter.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Counter
   }
 }
 </script>
---
+++ src/components/Counter.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <h2>Counter component</h2>
+        <div class="card-panel teal lighten-2 " id="counter">
+            <h4>Counter: {{ counter }}</h4>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Counter', 
+        data: function() {
+             return {
+                counter: 0
+            }
+        },
+        mounted() {
+            setInterval(() => {
+                this.counter++
+            }, 1000)
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
+
